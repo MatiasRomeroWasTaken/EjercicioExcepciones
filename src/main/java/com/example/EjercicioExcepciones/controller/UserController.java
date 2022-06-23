fd448fbe9f54d1b20fc32c2a3320e9be9978947a
MatiasRomeroWasTaken <matiasromero@santandertecnologia.com.ar>
2022-06-23 10:02:54 -0300

    setting controller

--- src/main/java/com/example/EjercicioExcepciones/controller/UserController.java
+++ src/main/java/com/example/EjercicioExcepciones/controller/UserController.java
@@ -1,6 +1,9 @@
 package com.example.EjercicioExcepciones.controller;
 
-import com.example.EjercicioExcepciones.entities.UserEntity;
+import com.example.EjercicioExcepciones.dtos.ResponseDTO;
+import com.example.EjercicioExcepciones.dtos.UserDTO;
+import com.example.EjercicioExcepciones.service.UserServiceImpl;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -9,4 +12,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class UserController {
 
+    private UserServiceImpl userService;
+
+    public UserController() {
+        this.userService = new UserServiceImpl();
+    }
+
+    @PostMapping ("/api/user")
+    public ResponseEntity<ResponseDTO> createUser(@RequestBody UserDTO userDTO){
+        return new ResponseEntity<ResponseDTO>(new ResponseDTO("Se ha creado el usuario"),HttpStatus.CREATED);
+    }
 }
---
+++ src/main/java/com/example/EjercicioExcepciones/dtos/ResponseDTO.java
@@ -0,0 +1,8 @@
+package com.example.EjercicioExcepciones.dtos;
+
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
+public class ResponseDTO {
+    private String message;
+}
